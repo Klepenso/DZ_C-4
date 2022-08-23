1ad4d780e3b374cc59f85e07f21c0a2f317a96a2
Ksenia Tovmasyan <ksenia.tovmasyan@yandex.ru>
2022-08-23 23:53:40 +0700

    Task 29 done

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+int lengthArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lengthArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(-100, 100);
+    Console.Write(randomArray[i] + " ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
